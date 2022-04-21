b0fb1f7b68ef8512dd1622a46a271bd916efecdf
Weerinrada Wongrin <47111796+Weerinrada@users.noreply.github.com>
2022-04-21 17:31:58 +0700

    Add files via upload

---
+++ img2text.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import easyocr
+
+#read img
+img = cv2.imread("filename.png")
+plt.imshow(img)
+
+#convert orignal image file into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+##convert grayscale with thresold
+th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
+
+print(threshed)
+
+#run reader with thai ('th') and english ('en')
+reader = easyocr.Reader(['th','en'], gpu = False) # this needs to run only once to load the model into memory
+
+#OCR with reader from easyocr module
+text = reader.readtext(threshed)
+print(text)
+
+#define a function to draw bounding boxes
+def draw_boxes(image, bounds, color = 'blue', width = 2):
+    draw = ImageDraw.Draw(image)
+    for bound in bounds:
+        p0, p1, p2, p3 = bound[0]
+        draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
+    return image
+
+im = Image.open("filename")
+draw_boxes(im, text)
